b8c21bf273241e0ae1aaee9a1b17094e479b22eb
Rashmi Venkatesan <rashmiofficial10@gmail.com>
2026-07-30 17:03:56 +0100

    Added login page object

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    username = (By.ID, "username")
+    password = (By.ID, "password")
+    login_button = (By.ID, "login")
+
+    def enter_username(self, username):
+        self.driver.find_element(*self.username).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(*self.password).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(*self.login_button).click()
