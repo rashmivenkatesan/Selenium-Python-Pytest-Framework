7b62d0a3d3a051c7544a7031d7fad16d180c2450
Rashmi Venkatesan <rashmiofficial10@gmail.com>
2026-07-31 18:49:53 +0100

    Added screenshot capture on test failure

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from selenium import webdriver
 
@@ -10,3 +11,21 @@ def setup():
     yield driver
 
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+
+        driver = item.funcargs.get("setup")
+
+        if driver:
+            os.makedirs("screenshots", exist_ok=True)
+            screenshot_path = os.path.join(
+                "screenshots",
+                f"{item.name}.png"
+            )
+            driver.save_screenshot(screenshot_path)
