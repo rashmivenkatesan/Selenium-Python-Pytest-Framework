0bb1cb6660d35607b9e413b57dffb69796b2a135
Rashmi Venkatesan <rashmiofficial10@gmail.com>
2026-08-05 12:18:48 +0100

    Added JSON data driven testing support

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,19 @@
 from pages.login_page import LoginPage
+from utilities.json_reader import read_json
 
 
 def test_login(setup):
 
     driver = setup
 
+    login_data = read_json("testdata/login_data.json")
+
     driver.get("https://www.saucedemo.com/")
 
     login = LoginPage(driver)
 
-    login.enter_username("standard_user")
-    login.enter_password("secret_sauce")
+    login.enter_username(login_data["username"])
+    login.enter_password(login_data["password"])
     login.click_login()
 
     assert "inventory" in driver.current_url
